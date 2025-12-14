ddc634c2c1eee71a1401eb4f3a8842ceeb2f70c1
Rvp0310 <rutujaparab55319@gmail.com>
2025-12-14 16:07:02 +0530

    Update view to show problem distribution per topic

--- dsa-tools/count_overall_problems.py
+++ dsa-tools/count_overall_problems.py
@@ -2,6 +2,7 @@ import os
 import re
 
 BASE = os.path.abspath(os.path.join(os.getcwd(), '../DSA_solved_problems'))
+
 difficulty = {
     'Easy': 0,
     'Medium': 0,
@@ -9,18 +10,23 @@ difficulty = {
 }
 
 def extract_total(readme):
-    total = 0
     with open(readme, 'r', encoding='utf-8') as f:
         for line in f:
             if line.startswith("**Total Solved**:"):
                 total = int(line.split(":")[1].strip())
             if line.startswith("- Easy: "):
-                difficulty['Easy'] += int(line.split(":")[1].strip())
+                val = int(line.split(":")[1].strip())
+                difficulty['Easy'] += val
+                easy = val
             if line.startswith("- Medium: "):
-                difficulty['Medium'] += int(line.split(":")[1].strip())
+                val = int(line.split(":")[1].strip())
+                difficulty['Medium'] += val
+                medium = val
             if line.startswith("- Hard: "):
-                difficulty['Hard'] += int(line.split(":")[1].strip())
-    return total
+                val = int(line.split(":")[1].strip())
+                difficulty['Hard'] += val
+                hard = val
+    return (easy, medium, hard, total)
 
 def update_readme():
     topics_done = {}
@@ -28,22 +34,22 @@ def update_readme():
         path = os.path.join(BASE, topic_folder)
         readme_path = os.path.join(path, "README.md")
         if os.path.isdir(path) and os.path.exists(readme_path):
-            total = extract_total(readme_path) 
-            if total > 0:
-                topics_done[topic_folder] = total
+            counts = extract_total(readme_path) 
+            if int(counts[-1]) > 0:
+                topics_done[topic_folder] = counts
     
     target = os.path.join(BASE, "README.md")
     with open(target, 'r', encoding='utf-8') as f:
         existing_content = f.read().split('---')[0].strip()  
 
-    topic_list = [f"- [**{count}**] {topic} (./{topic}/README.md)" for topic, count in topics_done.items()]
-    topics_section = "\n".join(topic_list)
+    topic_list = [f"| *{topic}* | **{counts[0]}** | **{counts[1]}** | **{counts[2]}** | **{counts[3]}** | ./{topic}/README.md |" for topic, counts in topics_done.items()]
+    topics_section = ("\n| Topic | Easy | Medium | Hard | Total | Path |\n|------|------|--------|------|-------|-------|\n" +"\n".join(topic_list))
     new_content = f"""
 ---
 
 ## Progress Overview
 
-- Total Problems Solved: {sum(topics_done.values())}
+- Total Problems Solved: {sum(counts[3] for counts in topics_done.values())}
     - Easy: {difficulty['Easy']}
     - Medium: {difficulty['Medium']}
     - Hard: {difficulty['Hard']}
